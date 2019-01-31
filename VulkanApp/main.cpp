3a9d3913a2aec93181e2b6ecc67645541b057635
Tomas Arriagada <taarriag@dhcp-100-98-228-239.mtv.corp.google.com>
2019-01-31 09:55:22 -0800

    Initial Commit

---
+++ VulkanApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VulkanApp
+//
+//  Created by Tomas Arriagada on 1/31/19.
+//  Copyright © 2019 Tomas Arriagada. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
